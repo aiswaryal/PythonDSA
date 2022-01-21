b475e0696afa40596a55a46b3ef805a2457d6a7b
AISWARYA LENIN <64835600+aiswaryal@users.noreply.github.com>
2022-01-22 01:39:41 +0530

    Create First Bad Version.py

---
+++ BinarySearch/First Bad Version.py
@@ -0,0 +1,28 @@
+"""You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+ """
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        lo = 1
+        hi = n
+        
+        if isBadVersion(lo):
+            return lo
+        
+   
+        
+        while(lo <= hi):
+            mid = lo + (hi-lo)//2
+            val = isBadVersion(mid)
+            if (val and isBadVersion(mid - 1) == False):
+                return mid
+            
+            elif val == False:
+                lo = mid + 1
+            else:
+                hi = mid -1
+
