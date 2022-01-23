e86f35149a4db01fa0529cc89fc870de57e97162
AISWARYA LENIN <64835600+aiswaryal@users.noreply.github.com>
2022-01-23 16:01:32 +0530

    Create Sqrt(x).py

---
+++ BinarySearch/Sqrt(x).py
@@ -0,0 +1,44 @@
+"""iven a non-negative integer x, compute and return the square root of x.
+
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+
+ 
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+ 
+
+Constraints:
+
+0 <= x <= 231 - 1"""
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        
+        if x <= 1:
+            return x
+        
+        start = 2
+        end = x 
+        middle = 0
+        while(start <= end):
+            middle = start + (end - start)//2 
+            
+            if(middle == x/middle):
+                return middle
+            elif(middle < x/middle):
+                start = middle +1 
+            else:
+                end = middle - 1
+        if start > x/start:
+            return start - 1
+        
+        return start
